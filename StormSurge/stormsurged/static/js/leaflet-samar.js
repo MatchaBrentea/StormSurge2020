613c26e025c30f11ee417c63f7243282e1644a54
MatchaBrentea <brzaguirre@up.edu.ph>
2020-03-16 11:39:09 +0800

    added SMS sending feature

---
+++ StormSurge/stormsurged/static/js/leaflet-samar.js
@@ -0,0 +1,91 @@
+var url_str = "https://raw.githubusercontent.com/MatchaBrentea/stormsurgefiles/master/"
+var kml_docs = ["maxelev_Haiyan_1_4_Leyte.geojson","maxelev_Haiyan_1_4_Samar.geojson"];
+var i;
+var loc=[];
+var notif_arr=[];
+
+var bar = $(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "https://raw.githubusercontent.com/MatchaBrentea/stormsurgefiles/master/tacloban_notif_coord.csv",
+        dataType: "text",
+        success: function(data) {processData(data);}
+    });
+});
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines=[];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                tarr.push(headers[j]+":"+data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+    
+    for (var i=0; i<=lines.length;i++){
+        var to_input = "<h1>STORM SURGE WARNING</h1>"
+        + "<p style = 'color:red';''font-weight='bold'>"+(lines[i][0].substring(5,lines[i][0].length))
+        + ", Tacloban City, 6500 Leyte, Philippines</p>"
+        + "Expect a surge as early as "
+        + ((parseFloat((lines[i][4].substring(4,lines[i][4].length))))/60).toString()
+        + " hours from now which can reach max height of "
+        + (lines[i][3].substring(6,lines[i][3].length))
+        + " meter(s) at "
+        + (lines[i][0].substring(5,lines[i][0].length))
+        + ". A contact your local government units for for their corresponding evacuation procedure"
+        loc.push([to_input]);
+        loc[i].push(lines[i][1].substring(7,lines[i][1].length));
+        loc[i].push(lines[i][2].substring(7,lines[i][2].length));
+    }
+
+    
+}
+
+var barangays = $.ajax({
+url: url_str.concat(kml_docs[1]),
+dataType: "json",
+success: console.log("Barangay data successfully loaded."),
+error: function (xhr) {
+    alert(xhr.statusText)
+}
+})
+
+
+function style(feature) {
+return {
+    weight: 0,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7,
+    fillColor: feature.properties.fill
+    };
+}
+
+$.when(barangays).done(function() {
+    var map = L.map('map').setView([11.27794, 125.07323], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var sidebar = L.control.sidebar('sidebar').addTo(map);
+    
+    var kybarangays = L.geoJSON(barangays.responseJSON,{style:style} ).addTo(map);
+    
+
+    for (var i = 0; i < loc.length; i++) {
+        marker = new L.marker([loc[i][1],loc[i][2]])
+        .bindPopup(loc[i][0])
+        .addTo(map);
+    }
+
+});
---
+++ StormSurge/stormsurged/static/js/promotexter.js
@@ -0,0 +1,31 @@
+
+function text(){
+    var request = new XMLHttpRequest();
+
+    var apiKey = 'ddb86e90de561fbfbe516e1654093998';
+    var apiSecret=  '493c73fae59a955b07ae33254188bec7';
+    var from = 'SMSLSurge';
+    var to = ['09088860685','09298146675'];
+    var text = 'This is a warning for StormSurge!';
+
+    var arr_len = to.length;
+    for(var i=0; i<=arr_len-1;i++){
+        var url = `https://api.promotexter.com/sms/send?apiKey=${apiKey}&apiSecret=${apiSecret}&from=${from}&to=${to[i]}&text=${text}`;
+
+        request.open('POST', `${url}`);
+
+        request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            console.log('Status:', this.status);
+            console.log('Headers:', this.getAllResponseHeaders());
+            console.log('Body:', this.responseText);
+        }
+        };
+
+        request.send();   
+        // alert(`${url}`);
+    }
+
+    alert("Early Warning SMS Sent!");
+
+}
