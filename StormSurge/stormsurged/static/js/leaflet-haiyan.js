655f57dfa80189fc456d149de7cb76a61ac86377
MatchaBrentea <brzaguirre@up.edu.ph>
2020-05-11 21:13:05 +0800

    finished

--- StormSurge/stormsurged/static/js/leaflet-haiyan.js
+++ StormSurge/stormsurged/static/js/leaflet-haiyan.js
@@ -1,11 +1,3 @@
-var url_str = "https://raw.githubusercontent.com/MatchaBrentea/stormsurgefiles/master/"
-var kml_docs = ["inundation_Haiyan.geojson"];
-var i;
-var loc=[];
-var notif_arr=[];
-
-
-
 map = L.map('map').setView([11.2142199211672, 125.009497200035], 12);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -15,73 +7,61 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var sidebar = L.control.sidebar('sidebar').addTo(map);
 
 
-
 var allResults = [];
-var triangles=[];
+var triangles = [];
 
-    
 
-    var kybarangays = L.geoJSON(barangays.responseJSON,{style:style} ).addTo(map);
+Papa.parse('https://raw.githubusercontent.com/StormSurgeApp/StormSurgeFiles/master/kml%20files/fort.14', {
+        download: true,
+        dynamicTyping: true,
 
-    Papa.parse("https://raw.githubusercontent.com/MatchaBrentea/stormsurgefiles/master/timeseriesHaiyan_1_0_new.txt", {
-    download: true,
-    step: function(row) {
-        console.log("Row:", row.data);
-    },
-    complete: function() {
-        console.log("All done!");
+        complete: function(results) {
 
-    Papa.parse('https://raw.githubusercontent.com/StormSurgeApp/StormSurgeFiles/master/kml%20files/fort.14', {
-    download: true,
-    dynamicTyping: true,
+            data=results.data;
+            NE=data[1][0];
+            NP=data[1][1];
+            var j;
+            for(j=0;j<NE;j++){
+            var temp = [
+                [data[data[NP+2+j][2]+1][2], data[data[NP+2+j][2]+1][1]],
+                [data[data[NP+2+j][3]+1][2], data[data[NP+2+j][3]+1][1]],
+                [data[data[NP+2+j][4]+1][2], data[data[NP+2+j][4]+1][1]]
+            ];
+             triangle=L.polygon(temp, {
+                weight: 0.1,
+                opacity: 1,
+                color: 'white',
+                fillOpacity: 0.1,
+                fillColor: 'blue'
+                }).addTo(map);
+            }
 
-    complete: function(results) {
-
-    data=results.data;
-    NE=data[1][0];
-    NP=data[1][1];
-    var j;
-    for(j=0;j<NE;j++){
-    var temp = [
-        [data[data[NP+2+j][2]+1][2], data[data[NP+2+j][2]+1][1]],
-        [data[data[NP+2+j][3]+1][2], data[data[NP+2+j][3]+1][1]],
-        [data[data[NP+2+j][4]+1][2], data[data[NP+2+j][4]+1][1]]
-    ];
-    var triangle=L.polygon(temp);
-    triangle.addTo(map);
-    triangles.push(temp);
-    }
-
-    }
-    });
-
+        }
+        });
 
 
 Papa.parse('https://raw.githubusercontent.com/MatchaBrentea/stormsurgefiles/master/timeseriesHaiyan_1_0_new.txt', {
     download: true,
     dynamicTyping: true,
-    worker: true,
     complete: function(results) {
         notif_arr=results.data
-         alert("done");
-         console.log("done");
+        console.log("done");
         // main algo
         time_series=results.data;
         var frame_num = (time_series.length - 1) / 9000;
         original_time=time_offset;
-
         var el = document.getElementById("startBtn");
         if (el.addEventListener)
             el.addEventListener("click", animateKML.bind(this,time_series,time_offset), false);
         else if (el.attachEvent)
-            el.attachEvent('onclick', animateKML.bind(this,time_series,time_offset));  
+            el.attachEvent('onclick', animateKML.bind(this,time_series,time_offset));
+
 
         var el2 = document.getElementById("stopBtn");
         if (el2.addEventListener)
             el2.addEventListener("click", cancelAnimation, false);
         else if (el2.attachEvent)
             el2.attachEvent('onclick', cancelAnimation)
-
     }
 });
     
@@ -90,38 +70,74 @@ var anim;
 var time_offset;
 var original_time;
 time=0;
-time_offset = new Date('2013','11' - 1 , '4', '6', '0' + 480, 0, 0)
+
+time_offset = new Date('2013','11' - 1 , '4', '6', '0' + 480, 0, 0);
+
 document.getElementById("AnimationTime").innerHTML = '<p><font size=4> Typhoon Haiyan in Tacloban City </font></p>' + '<p><font size=4>'+time_offset.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })+'</font></p>'
+
+var triangle;
+var count = 0;
 
 function animateKML(time_series,time_offset) 
 {
+
+    Papa.parse('https://raw.githubusercontent.com/StormSurgeApp/StormSurgeFiles/master/kml%20files/fort.14', {
+        download: true,
+        dynamicTyping: true,
+
+        complete: function(results) {
+
+            data=results.data;
+            NE=data[1][0];
+            NP=data[1][1];
+            var j;
+            for(j=0;j<NE;j++){
+            var temp = [
+                [data[data[NP+2+j][2]+1][2], data[data[NP+2+j][2]+1][1]],
+                [data[data[NP+2+j][3]+1][2], data[data[NP+2+j][3]+1][1]],
+                [data[data[NP+2+j][4]+1][2], data[data[NP+2+j][4]+1][1]]
+            ];
+            triangles.push(temp);
+            }
+
+        }
+        });
+
     if (time>(time_series.length/triangles.length)-1){
         time = 0;
-        //console.log(time,time_offset);
+        time_offset.setMinutes(time_offset.getMinutes()- 1440)
       } 
+
     for(var k=0;k<triangles.length;k++){
+        
 
-        setInterval(function(){
-            function style() {
-                return {
-                    weight: 0,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.7,
-                    fillColor: time_series[time*triangles.length+k][0]
-                    };
-                }
-            var kybarangays = L.geoJSON(barangays.responseJSON,{style:style} ).addTo(map);
+        triangle=L.polygon(triangles[k], {
+                weight: 0.1,
+                opacity: 1,
+                color: 'white',
+                fillOpacity: 0.7,
+                fillColor: time_series[time*triangles.length+k][1]
+                }).addTo(map)
 
-        },3000);
+    //     for(i in map._layers) {
+    //     if(map._layers[i]._path != undefined) {
+    //         try {
+    //             map.removeLayer(map._layers[i]);
+    //         }
+    //         catch(e) {
+    //             console.log("problem with " + e + map._layers[i]);
+    //         }
+    //     }
+    // }       
+        
 
     }
+
     time++;
     time_offset.setMinutes(time_offset.getMinutes()+ 5)
 
-    document.getElementById("AnimationTime").innerHTML = '<p><font size=4> Typhoon Rammasun in Bataan </font></p>' + '<p><font size=4>'+time_offset.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })+'</font></p>'
-    map.removeLayer(kybarangays);
+
+    document.getElementById("AnimationTime").innerHTML = '<p><font size=4> Typhoon Haiyan in Tacloban City </font></p>' + '<p><font size=4>'+time_offset.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })+'</font></p>'
 
     anim=requestAnimationFrame(animateKML.bind(this,time_series,time_offset));
     
--- StormSurge/stormsurged/static/js/leaflet-rammasun.js
+++ StormSurge/stormsurged/static/js/leaflet-rammasun.js
@@ -1,8 +1,4 @@
-var url_str = "https://raw.githubusercontent.com/MatchaBrentea/stormsurgefiles/master/"
-var kml_docs = ["maxelev_Haiyan_2.geojson","maxelev_Haiyan_1_4_Leyte.geojson","maxelev_Haiyan_1_4_Samar.geojson","maxelev_Rammasun_1.geojson"];
-var i;
-var loc=[];
-var notif_arr=[];
+
 
 
 
@@ -19,29 +15,33 @@ var allResults = [];
 var triangles = [];
 
 
-// Papa.parse('https://raw.githubusercontent.com/StormSurgeApp/StormSurgeFiles/master/fort.14', {
-// download: true,
-// dynamicTyping: true,
+Papa.parse('https://raw.githubusercontent.com/StormSurgeApp/StormSurgeFiles/master/fort.14', {
+        download: true,
+        dynamicTyping: true,
 
-// complete: function(results) {
+        complete: function(results) {
 
-//     data=results.data;
-//     NE=data[1][0];
-//     NP=data[1][1];
-//     var j;
-//     for(j=0;j<NE;j++){
-//     var temp = [
-//         [data[data[NP+2+j][2]+1][2], data[data[NP+2+j][2]+1][1]],
-//         [data[data[NP+2+j][3]+1][2], data[data[NP+2+j][3]+1][1]],
-//         [data[data[NP+2+j][4]+1][2], data[data[NP+2+j][4]+1][1]]
-//     ];
-//     var triangle=L.polygon(temp);
-//     // triangle.addTo(map);
-//     triangles.push(temp);
-//     }
+            data=results.data;
+            NE=data[1][0];
+            NP=data[1][1];
+            var j;
+            for(j=0;j<NE;j++){
+            var temp = [
+                [data[data[NP+2+j][2]+1][2], data[data[NP+2+j][2]+1][1]],
+                [data[data[NP+2+j][3]+1][2], data[data[NP+2+j][3]+1][1]],
+                [data[data[NP+2+j][4]+1][2], data[data[NP+2+j][4]+1][1]]
+            ];
+             triangle=L.polygon(temp, {
+                weight: 0.1,
+                opacity: 1,
+                color: 'white',
+                fillOpacity: 0.1,
+                fillColor: 'blue'
+                }).addTo(map);
+            }
 
-// }
-// });
+        }
+        });
 
 
 Papa.parse('https://raw.githubusercontent.com/StormSurgeApp/StormSurgeFiles/master/kml%20files/timeseriesRammasun_1_0_new.txt', {
@@ -58,7 +58,8 @@ Papa.parse('https://raw.githubusercontent.com/StormSurgeApp/StormSurgeFiles/mast
         if (el.addEventListener)
             el.addEventListener("click", animateKML.bind(this,time_series,time_offset), false);
         else if (el.attachEvent)
-            el.attachEvent('onclick', animateKML.bind(this,time_series,time_offset));  
+            el.attachEvent('onclick', animateKML.bind(this,time_series,time_offset));
+
 
         var el2 = document.getElementById("stopBtn");
         if (el2.addEventListener)
@@ -78,27 +79,8 @@ time_offset = new Date('2014','10' - 1 , '14', '0', '0' + 480, 0, 0)
 
 document.getElementById("AnimationTime").innerHTML = '<p><font size=4> Typhoon Rammasun in Bataan </font></p>' + '<p><font size=4>'+time_offset.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })+'</font></p>'
 
-var barangays = $.ajax({
-    url: url_str.concat(kml_docs[3]),
-    dataType: "json",
-    success: console.log("Barangay data successfully loaded."),
-    error: function (xhr) {
-        alert(xhr.statusText)
-    }
-    })
-
-    function style() {
-        return {
-            weight: 0,
-            opacity: 1,
-            color: 'white',
-            dashArray: '3',
-            fillOpacity: 0.7,
-            fillColor: 'white'
-            };
-        }
-
 var triangle;
+var count = 0;
 
 function animateKML(time_series,time_offset) 
 {
@@ -119,8 +101,6 @@ function animateKML(time_series,time_offset)
                 [data[data[NP+2+j][3]+1][2], data[data[NP+2+j][3]+1][1]],
                 [data[data[NP+2+j][4]+1][2], data[data[NP+2+j][4]+1][1]]
             ];
-            // triangle=L.polygon(temp);
-            // triangle.addTo(map);
             triangles.push(temp);
             }
 
@@ -133,36 +113,38 @@ function animateKML(time_series,time_offset)
       } 
 
     for(var k=0;k<triangles.length;k++){
+        
 
-        setInterval(function(){
-            // function style() {
-            //     return {
-            //         weight: 0,
-            //         opacity: 1,
-            //         color: 'white',
-            //         dashArray: '3',
-            //         fillOpacity: 0.7,
-            //         fillColor: time_series[time*triangles.length+k][0]
-            //         };
-            //     }
-            // var kybarangays = L.geoJSON(barangays.responseJSON,{style:style} ).addTo(map);
+        triangle=L.polygon(triangles[k], {
+                weight: 0.1,
+                opacity: 1,
+                color: 'white',
+                fillOpacity: 0.7,
+                fillColor: time_series[time*triangles.length+k][1]
+                }).addTo(map)
 
-            // console.log(triangles[k]);
-            triangle=L.polygon(triangles[k]);
-            triangle.addTo(map);
-        },2000);
+    //     for(i in map._layers) {
+    //     if(map._layers[i]._path != undefined) {
+    //         try {
+    //             map.removeLayer(map._layers[i]);
+    //         }
+    //         catch(e) {
+    //             console.log("problem with " + e + map._layers[i]);
+    //         }
+    //     }
+    // }       
+        
 
-        // console.log(time_series[time*triangles.length+k][0])
     }
 
     time++;
     time_offset.setMinutes(time_offset.getMinutes()+ 5)
 
+
     document.getElementById("AnimationTime").innerHTML = '<p><font size=4> Typhoon Rammasun in Bataan </font></p>' + '<p><font size=4>'+time_offset.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })+'</font></p>'
-    // map.removeLayer(triangle);
+
 
     anim=requestAnimationFrame(animateKML.bind(this,time_series,time_offset));
-    
     
   }    
 
